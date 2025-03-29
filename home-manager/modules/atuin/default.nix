b1e5134644e427896ba8c94cef5e174b336e4e99
Mohammad Abdolirad <m.abdolirad@gmail.com>
2025-03-29 08:37:06 +0100

    Update "atuin" settings

--- home-manager/modules/atuin/default.nix
+++ home-manager/modules/atuin/default.nix
@@ -8,10 +8,18 @@
   programs.atuin = {
     flags = ["--disable-up-arrow"];
     enable = true;
+    package = pkgs.unstable.atuin;
     settings = {
+      inline_height = 25;
+      show_help = false;
+      show_tabs = false;
       auto_sync = false;
       style = "compact";
-      theme.name = "marine";
+      workspaces = true;
+      common_prefix = [
+        "sudo"
+        "_"
+      ];
       common_subcommands = [
         "composer"
         "docker"
