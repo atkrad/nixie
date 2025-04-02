b19db69464651f065381ccabe84cf16c3bfc6e57
Mohammad Abdolirad <m.abdolirad@gmail.com>
2025-04-02 09:14:23 +0200

    Update "cato-client" to v5.4.0.1006

--- pkgs/cato-client/default.nix
+++ pkgs/cato-client/default.nix
@@ -4,15 +4,15 @@
   lib,
   ...
 }: let
-  version = "5.3.0.382";
+  version = "5.4.0.1006";
 in
   pkgs.stdenv.mkDerivation {
     name = "cato-client";
 
     src = pkgs.fetchurl {
       inherit version;
-      url = "https://clients.catonetworks.com/linux/5.3.0.382/cato-client-install.deb";
-      sha256 = "sha256-2vjMSwGaVwzHr0NjqEKOZHwZu99vW/Ro7WK5YK8T1BQ=";
+      url = "https://clients.catonetworks.com/linux/5.4.0.1006/cato-client-install.deb";
+      sha256 = "sha256-3RyiDgfWL4vA/pUyOigmuK+SV1Q96Y65HVERPeadWWY=";
     };
 
     nativeBuildInputs = with pkgs; [
